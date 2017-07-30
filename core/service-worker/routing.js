9b390adbad532881d078bedddca9e8c33fcf7a50
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-07-30 19:10:51 +0800

    my firebase mixin is a little bit more stable
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- core/service-worker/routing.js
+++ core/service-worker/routing.js
@@ -1,18 +1,20 @@
-console.log('https://' + app.firebaseId + '.firebaseio.com/:json+')
-const route = new workbox.routing.ExpressRoute({
+router.registerRoute({route: new workbox.routing.ExpressRoute({
   path: 'https://' + app.firebaseId + '.firebaseio.com/:json+',
-  handler: ({event, params, url}) => {
-    // console.log(caches, event, params, url, event.request)
-    return fetch(url.href).then(function (response) {
-      caches.open(app.shortTitle).then(function (cache) {
-        cache.put(event.request, response)
-      })
-      return response.clone()
-    }).catch(function (err) {
-      console.log(url.href, caches.match(url.href))
-      return caches.match(url.href)
-    })
-  }
-})
+  handler: function (obj) {
+    var event = obj.event
+    var params = obj.params
+    var url = obj.url
 
-router.registerRoute({route: route})
+    return fetch(url.href)
+      .then(function (response) {
+        caches.open(app.shortTitle).then(function (cache) {
+          cache.put(event.request, response)
+        })
+        return response.clone()
+      })
+      .catch(function (err) {
+        console.log(err, url.href, caches.match(url.href))
+        return caches.match(url.href)
+      })
+  }
+})})
