ade390708fe51aea47f579669993054a46cc2fc3
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-08-01 21:20:02 +0800

    finalized polyblog
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- core/gulp/tasks/compile-opts.js
+++ core/gulp/tasks/compile-opts.js
@@ -1,14 +1,14 @@
 const gulp = require('gulp')
-const gulpif = require('gulp-if')
+// const gulpif = require('gulp-if')
 const correctBowerPath = require('../utils/correct-bower-path')
-const injectReducers = require('../utils/inject-reducers')
+// const injectReducers = require('../utils/inject-reducers')
 const {destinationFolder, sources} = require('../utils/utils.js')
 
 gulp.task('compile-opts', () => {
   return gulp.src([sources.opts])
     .pipe(correctBowerPath())
-    .pipe(gulpif((file) => {
-      return file.path.indexOf('state-manager.html') >= 0 && /\.html$/.test(file.path)
-    }, injectReducers()))
+    // .pipe(gulpif((file) => {
+    //   return file.path.indexOf('state-manager.html') >= 0 && /\.html$/.test(file.path)
+    // }, injectReducers()))
     .pipe(gulp.dest(`${destinationFolder()}/opts`))
 })
